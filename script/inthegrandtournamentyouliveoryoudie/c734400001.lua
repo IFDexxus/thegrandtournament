2fe1b5b3ba066e425191dcffefd6e9294736d009
IFDexxus <thathomestuckguy@gmail.com>
2020-04-18 17:11:49 +0100

    ACTUALLY fixed Ryuga's Ring

--- script/inthegrandtournamentyouliveoryoudie/c734400001.lua
+++ script/inthegrandtournamentyouliveoryoudie/c734400001.lua
@@ -33,73 +33,23 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 	Duel.Hint(HINT_CARD,0,id)
 	--aroma effect
-	--cannot trigger
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_TRIGGER)
-	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-	e2:SetTargetRange(0,0xa)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_SPELL))
-	Duel.RegisterEffect(e2,tp)
-	--cannot activate
-	local e3=Effect.CreateEffect(e:GetHandler())
-	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e3:SetTargetRange(0,1)
-	e3:SetValue(s.aclimit)
-	e3:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e3,tp)
 	--disable
-	local e4=Effect.CreateEffect(e:GetHandler())
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e4:SetCode(EFFECT_DISABLE)
-	e4:SetTargetRange(0,LOCATION_SZONE)
-	e4:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_SPELL))
-	Duel.RegisterEffect(e4,tp)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_DISABLE)
+	e3:SetTargetRange(0,LOCATION_ONFIELD)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_SPELL))
+	c:RegisterEffect(e3)
 	--disable effect
-	local e5=Effect.CreateEffect(e:GetHandler())
-	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e5:SetCode(EVENT_CHAIN_SOLVING)
-	e5:SetOperation(s.disop)
-	Duel.RegisterEffect(e5,tp)
-	--disable trap monster
-	local e6=Effect.CreateEffect(e:GetHandler())
-	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e6:SetCode(EFFECT_DISABLE_TRAPMONSTER)
-	e6:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e6:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_SPELL))
-	Duel.RegisterEffect(e6,tp)
-	--immune
-	local e7=Effect.CreateEffect(e:GetHandler())
-	e7:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e7:SetCode(EFFECT_IMMUNE_EFFECT)
-	e7:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-	e7:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
-	e7:SetTarget(s.etarget)
-	e7:SetValue(s.efilter)
-	Duel.RegisterEffect(e7,tp)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_CHAIN_SOLVING)
+	e4:SetOperation(s.disop)
+	c:RegisterEffect(e4)
 end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	local tl=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
-	if tl==LOCATION_SZONE and re:IsActiveType(TYPE_SPELL) then
+	if rp~=tp and tl==LOCATION_SZONE and re:IsActiveType(TYPE_SPELL) then
 		Duel.NegateEffect(ev)
 	end
-end
-function s.aclimit(e,re,tp)
-	return re:GetHandler():IsType(TYPE_SPELL)
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetDecktopGroup(e:GetHandler():GetControler(),1)
-	if g:GetFirst():GetFlagEffect(id)==0 then
-		Duel.ConfirmDecktop(e:GetHandler():GetControler(),1)
-		g:GetFirst():RegisterFlagEffect(id,RESET_EVENT+0x1fe0000,0,1)
-	end
-end
-function s.etarget(e,c)
-	local ec=e:GetHandler()
-	return c:IsType(TYPE_SPELL) and ec and c:GetControler()==ec:GetControler()
-end
-function s.efilter(e,re)
-	return re:GetHandler()==e:GetHandler()
 end
