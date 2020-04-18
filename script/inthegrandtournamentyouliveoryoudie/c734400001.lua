f56c0f592054c88643b77d9c869a3e840d7ff7d7
IFDexxus <thathomestuckguy@gmail.com>
2020-04-18 17:40:32 +0100

    Oh, THERE'S the problem.

--- script/inthegrandtournamentyouliveoryoudie/c734400001.lua
+++ script/inthegrandtournamentyouliveoryoudie/c734400001.lua
@@ -33,23 +33,43 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 	Duel.Hint(HINT_CARD,0,id)
 	--aroma effect
+	--cannot activate
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(0,1)
+	e2:SetValue(s.aclimit)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e2,tp)
 	--disable
-	local e3=Effect.CreateEffect(c)
+	local e3=Effect.CreateEffect(e:GetHandler())
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_DISABLE)
 	e3:SetTargetRange(0,LOCATION_ALL)
 	e3:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_SPELL))
-	c:RegisterEffect(e3)
+	c:RegisterEffect(e3,tp)
 	--disable effect
-	local e4=Effect.CreateEffect(c)
+	local e4=Effect.CreateEffect(e:GetHandler())
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e4:SetCode(EVENT_CHAIN_SOLVING)
 	e4:SetOperation(s.disop)
-	c:RegisterEffect(e4)
+	c:RegisterEffect(e4,tp)
+	--cannot trigger
+	local e5=Effect.CreateEffect(e:GetHandler())
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_CANNOT_TRIGGER)
+	e5:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e5:SetTargetRange(0,0xa)
+	e5:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_SPELL))
+	c:RegisterEffect(e5,tp)
 end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	local tl=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
 	if rp~=tp and tl==LOCATION_SZONE and re:IsActiveType(TYPE_SPELL) then
 		Duel.NegateEffect(ev)
 	end
+end
+function s.aclimit(e,re,tp)
+	return re:GetHandler():IsType(TYPE_SPELL)
 end
