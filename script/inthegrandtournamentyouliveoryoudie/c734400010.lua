593bb948effeadd0fd1ba04ef8b62ebe4421a4f1
IFDexxus <thathomestuckguy@gmail.com>
2020-04-19 23:31:02 +0100

    ???

--- script/inthegrandtournamentyouliveoryoudie/c734400010.lua
+++ script/inthegrandtournamentyouliveoryoudie/c734400010.lua
@@ -34,27 +34,22 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,0,id)
 	local c=e:GetHandler()
 	--Ground Collapse
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_FIELD)
-		e2:SetCode(EFFECT_DISABLE)
-		e2:SetTargetRange(LOCATION_MZONE,0)
-		e2:SetValue(2)
-		e2:SetTarget(s.distg)
-		e2:SetLabelObject(e1)
-		Duel.RegisterEffect(e2,tp)
-		local e3=Effect.CreateEffect(c)
-		e3:SetType(EFFECT_TYPE_FIELD)
-		e3:SetCode(EFFECT_DISABLE)
-		e3:SetTargetRange(LOCATION_SZONE,0)
-		e3:SetValue(2)
-		e3:SetTarget(s.distg)
-		e3:SetLabelObject(e1)
-		Duel.RegisterEffect(e3,tp)
+    c:RegisterEffect(e1)
+    --disable field
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_DISABLE_FIELD)
+	e2:SetTarget(s.target)
+    e2:SetOperation(s.disop)
+    e2:SetLabelObject(e1)
+    c:RegisterEffect(e2)
 end
-function s.con1(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(ep,id)~=0
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE+LOCATION_SZONE,PLAYER_NONE,0)>1 end
+    local dis=Duel.SelectDisableField(tp,2,LOCATION_MZONE+LOCATION_SZONE,0,c:GetSequence()==4)
+    Duel.Hint(HINT_ZONE,tp,dis)
+    e:SetLabel(dis)
 end
-function s.distg(e,c)
-	return c:GetSequence()==0 or c:GetSequence()==4
+function s.disop(e,tp)
+    return e:GetLabelObject():GetLabel()
 end
-
