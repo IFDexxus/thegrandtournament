4ad360da3e075cbecbdff02f55844c75ed3fc87e
KingYamato <58759661+KingYamato@users.noreply.github.com>
2020-04-23 16:01:18 -0300

    fix destruction damage

--- script/inthegrandtournamentyouliveoryoudie/c734400013.lua
+++ script/inthegrandtournamentyouliveoryoudie/c734400013.lua
@@ -25,46 +25,42 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 		end
 	e:SetLabel(1)
 end
+
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
 	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
 end
+
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 	Duel.Hint(HINT_CARD,0,id)
 	--opd register
 	Duel.RegisterFlagEffect(ep,id,0,0,0)
 	local c=e:GetHandler()
-		--damage
-		local e2=Effect.CreateEffect(c)
-		e2:SetCategory(CATEGORY_DESTROY)
-		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-		e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-		e2:SetCode(EVENT_DESTROYED)
-		e2:SetCondition(s.descon)
-		e2:SetTarget(s.destg)
-		e2:SetOperation(s.desop)
---		e2:SetLabelObject(e1)
-		Duel.RegisterEffect(e2,tp)
+	--damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCondition(s.descon)
+	e2:SetOperation(s.desop)
+	Duel.RegisterEffect(e2,tp)
 end
-function s.con1(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(ep,id)~=0
-end
+
 function s.cfilter(c,tp)
-	return c:IsReason(REASON_EFFECT) and c:IsPreviousControler(tp)
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsReason(REASON_EFFECT) and c:IsPreviousControler(tp)
 end
+
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return true end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(300)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,300)
+
+function s.damage(c, tp)
+	damage = math.max(c:GetTextAttack()/2, 0)
+	Duel.Damage(tp,damage,REASON_EFFECT)
 end
+
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Damage(p,d,REASON_EFFECT)
+	eg:ForEach(s.damage)
 end
 
