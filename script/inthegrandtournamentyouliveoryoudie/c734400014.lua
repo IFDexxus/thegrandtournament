aa4faf7a5c7de882bc7ab859115bf50ddc60b32e
IFDexxus <thathomestuckguy@gmail.com>
2020-04-26 20:31:44 +0100

    014 fix

--- script/inthegrandtournamentyouliveoryoudie/c734400014.lua
+++ script/inthegrandtournamentyouliveoryoudie/c734400014.lua
@@ -35,19 +35,12 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	--Ground Collapse
 	local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_FIELD)
-		e2:SetCode(EFFECT_FORCE_MZONE)
-		e2:SetRange(LOCATION_MZONE)
-		e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-		e2:SetTargetRange(1,0)
-		e2:SetValue(s.znval)
-		e2:SetLabelObject(e1)
-		Duel.RegisterEffect(e2,tp)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+    e2:SetTargetRange(LOCATION_MZONE,0)
+	Duel.RegisterEffect(e2,tp)
 end
 function s.con1(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFlagEffect(ep,id)~=0
-end
-function s.znval(e)
-	return ~(e:GetHandler()0x60)
 end
 
