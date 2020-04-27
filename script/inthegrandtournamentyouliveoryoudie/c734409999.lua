7610ce4608be3556decc3157280d967b7acb8fd4
KingYamato <58759661+KingYamato@users.noreply.github.com>
2020-04-27 14:16:14 -0300

    Add mid chain resolution functionality

--- script/inthegrandtournamentyouliveoryoudie/c734409999.lua
+++ script/inthegrandtournamentyouliveoryoudie/c734409999.lua
@@ -1,18 +1,45 @@
 --Filler Control
 local s,id=GetID()
 function s.initial_effect(c)
-    aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
-end
+    aux.AddSkillProcedure(c,2,false,nil,nil)
+	
+	local e1=Effect.CreateEffect(c)	
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetOperation(s.flipop)
+	c:RegisterEffect(e1)
+	
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetOperation(s.ignitionop)
+	Duel.RegisterEffect(e2, tp)
 
-function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-    return aux.CanActivateSkill(tp)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_CHAIN_SOLVING)
+	e3:SetOperation(s.chainop)
+	Duel.RegisterEffect(e3, tp)
 end
 
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
     Duel.Hint(HINT_CARD,tp,id)
-	Duel.RegisterFlagEffect(ep,id,0,0,0)
-	
+end
+
+function s.ignitionop(e,tp,eg,ep,ev,re,r,rp)
+	s.sort(tp)
+end
+
+function s.chainop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		s.sort(tp)
+	end
+end
+
+function s.sort(tp)
 	local group = Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,nil,nil)
 	if #group < 1 then return end
 
