8948b3d410d2cd31990f27057d6da63fbedd0fc9
IFDexxus <thathomestuckguy@gmail.com>
2020-05-05 11:02:22 +0100

    M

--- script/inthegrandtournamentyouliveoryoudie/c734400029.lua
+++ script/inthegrandtournamentyouliveoryoudie/c734400029.lua
@@ -27,6 +27,8 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	
     local c=e:GetHandler()
     local search=Duel.GetFirstMatchingCard
+    Duel.MoveSequence(search(s.IsHell,c:GetOwner(),LOCATION_DECK,0,nil), 1)
+    Duel.MoveSequence(search(s.IsHell,c:GetOwner(),LOCATION_DECK,0,nil), 1)
     Duel.MoveSequence(search(s.IsHell,c:GetOwner(),LOCATION_DECK,0,nil), 0)
     Duel.MoveSequence(search(s.IsBalthazar,c:GetOwner(),LOCATION_DECK,0,nil), 0)
     Duel.MoveSequence(search(s.IsCaspar,c:GetOwner(),LOCATION_DECK,0,nil), 0)
